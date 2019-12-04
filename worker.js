1315651a576d804cc46680620c210407fc05b949
Dione David <ddavid6@uic.edu>
2019-12-04 01:48:15 +0000

    commit

--- worker.js
+++ worker.js
@@ -1,7 +1,10 @@
-   
-var cacheName = 'app-v3';
-var appShellFiles = [
-      '/it202-bigproject',
+
+
+ self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/it202-bigproject',
         '/it202-bigproject/index.html/',
         '/it202-bigproject/feedback.html/',
          '/it202-bigproject/image.png/',
@@ -9,35 +12,34 @@ var appShellFiles = [
         '/it202-bigproject/assets/javascript/app.js',
          '/it202-bigproject/assets/css/reset.css',
          '/it202-bigproject/assets/css/style.css',
-];
-
-
-//installs all the files as back up
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(appShellFiles);
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+ e.request.url);
-      
-      return r || fetch(e.request).then(function(response) {
-          return caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
         });
+        return response;
+      }).catch(function () {
+        return caches.match('./image.png');
       });
-    })
-  );
+    }
+  }));
 });
-
- 
 
 
 
